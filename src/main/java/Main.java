3d38ad1f9763aaa3a16e165c775af2433861fa7d
RenzeBoerman <mail@renzeboerman.nl>
2024-03-01 12:19:56 +0100

    Main init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.util.Arrays;
+
 public class Main {
-
-
-
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(SerialPort.getCommPorts()));
+    }
 }
